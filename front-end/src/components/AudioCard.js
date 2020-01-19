2a50d3f28e4cba32ae5c21460d494668d8879fa9
Roberto Battaglia <battaroberto@gmail.com>
2020-01-19 11:25:02 +0000

    some shit

--- front-end/src/components/AudioCard.js
+++ front-end/src/components/AudioCard.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
+import clsx from 'clsx'
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import Collapse from '@material-ui/core/Collapse';
+import Container from '@material-ui/core/Container';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import PlayPauseButton from './PlayPauseButton';
+import TranscriptedAudio from './TranscriptedAudio';
 
 const useStyles = makeStyles(theme => ({
   card: {
     display: 'flex',
+    maxWidth: 600,
   },
   details: {
     display: 'flex',
@@ -33,6 +40,16 @@ const useStyles = makeStyles(theme => ({
     height: 38,
     width: 38,
   },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
 }));
 
 const AudioCard = ({ track }) => {
@@ -40,7 +57,11 @@ const AudioCard = ({ track }) => {
   const theme = useTheme();
   const [audio, setAudio] = useState(new Audio(track.downloadUrl));
   const [isPlaying, setIsPlaying] = useState(false);
+  const [expanded, setExpanded] = React.useState(false);
 
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
   const play = () => {
     console.log("yooo")
@@ -80,6 +101,21 @@ const AudioCard = ({ track }) => {
         image={track.pictureUrl}
         title={track.title}
       />
+      <CardActions disableSpacing>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <TranscriptedAudio></TranscriptedAudio>
+      </Collapse>
     </Card>
   );
 }
