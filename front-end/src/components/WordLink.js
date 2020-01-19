5603008d9ba865148719fd3206316a7514c30f57
Sam Harris <sam@jam-bc.com>
2020-01-19 13:40:27 +0000

    moved old unused files and styled words for comments and hovering

--- front-end/src/components/WordLink.js
+++ front-end/src/components/WordLink.js
@@ -1,14 +1,41 @@
 import React, { useState } from 'react';
 
-const WordLink = ({ word, wordIndex, hasComments, openComments }) =>{
+import { makeStyles } from '@material-ui/core/styles';
 
-  const trigger = () => {
-    openComments(wordIndex)
+const useStyles = makeStyles(theme => ({
+  word: {
+    '&:hover': {
+      cursor: 'pointer',
+      backgroundColor: 'black',
+      borderRadius: '3px',
+      color: 'white'
+    }
+  },
+  commentedWord: {
+    backgroundColor: 'green',
+    borderRadius: '3px',
+    color: 'white',
+    '&:hover': {
+      cursor: 'pointer',
+      backgroundColor: 'blue'
+    }
   }
+}));
+
+const WordLink = ({ word, wordIndex, hasComments, openComments }) => {
+  const classes = useStyles();
+  const trigger = () => {
+    openComments(wordIndex);
+  };
 
   return (
-    <span onClick={trigger}>{word} </span>
-  )
-}
+    <span
+      className={hasComments ? classes.commentedWord : classes.word}
+      onClick={trigger}
+    >
+      {`${word} `}
+    </span>
+  );
+};
 
 export default WordLink;
